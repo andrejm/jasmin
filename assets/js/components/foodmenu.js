a99ddced24923022aeff283a43a6cdb7d153a781
Andrej Minárik <hello@andrzej.sk>
2017-01-18 22:25:32 +0100

    slick - make the fucker work on desktop after changes to logic

--- assets/js/components/foodmenu.js
+++ assets/js/components/foodmenu.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function($) {
-
 	$('.js-menu-sections').responsiveTabs({
 		// all options: https://github.com/jellekralt/Responsive-Tabs
 	    startCollapsed: 'accordion',
@@ -10,106 +9,94 @@ jQuery(document).ready(function($) {
 	    // animationQueue: 'accordion',
 	    load: activateSlicks(),
 	    activate: function(event, tab){
-	    	// $(window).trigger('resize');
-	    	$('.js-menu-paging').slick('reinit');
-	    	$('.js-menu-pages').slick('reinit');
-	    	// console.log('reinitialize');
+
+	    	// console.log($(tab.selector));
+	    	var sliderOnTab = tab.selector.substr(1);
+
+	    	// console.log(sliderOnTab);
+
+	    	$('#slider-' + sliderOnTab).slick('getSlick').resize();
+	    	$('#pagination-' + sliderOnTab).slick('getSlick').resize();
+
+
+	    	//$('#slider-vinny-listok').slick('getSlick').reinit()
 	    }
 	});
-	
-	
+
 	function activateSlicks() {
 		var $slickPaginations = $( '.js-menu-paging' );
 
 		$slickPaginations.each( function( index, element ) {
-
-			var $paginationSlider = $(element);
-			var target = $paginationSlider.data('for-slider');
-			var $targetSlider = $('#slider-' + target);
-
-			// console.log( $targetSlider );
-			// console.log($paginationSlider);
-
-			$targetSlider.slick({
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				infinite: false,
-				arrows: false,
-				dots: false,
-				adaptiveHeight: true,
-				asNavFor: '#pagination-' + target
-			});
-
-			$paginationSlider.slick({
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				asNavFor: '#slider-' + target,
-				dots: false,
-				arrows: true,
-				centerMode: true,
-				infinite: false,
-				focusOnSelect: true,
-				// initialSlide: 1,
-				variableWidth: true,
-				mobileFirst : true,
-				responsive : [
-					{
-						breakpoint : 480,
-						settings : {
-							slidesToShow: 3,
-							centerMode: false,
-						}
-					},
-					{
-						breakpoint : 640, //medium breakpoint
-						settings : {
-							slidesToShow: 4,
-							centerMode: false,
-						}
-					},
-					{
-						breakpoint : 840,
-						settings : {
-							//bad, but working version: centermode false, slidesToShow 9, arrows false
-							//when arrows true, there are two arrows (button elements) generated in slick track and they are considered slides and they desynchronize synced sliders
-							centerMode: false,
-							slidesToShow: 9,
-							arrows: false,
-						}
-					}
-				]
-			});
-
+			activateSlick(element);
 		});
-		
 
-		// $('.js-menu-pages').slick({
+		return true;
+	}
+
+	function activateSlick(element) {
+		var $paginationSlider = $(element);
+		var target = $paginationSlider.data('for-slider');
+		var $targetSlider = $('#slider-' + target);
+
+		var targetSliderOptions = {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: false,
+			asNavFor: '#pagination-' + target,
+			adaptiveHeight: true
+		};
+
+		var paginationSliderOptions = {
+			asNavFor: '#slider-' + target,
+			slidesToShow: 7,
+			slidesToScroll: 1,
+			dots: false,
+			centerMode: true,
+			focusOnSelect: true,
+			arrows: true
+		};
+
+		$targetSlider.slick( targetSliderOptions );
+
+		// $paginationSlider.slick({
 		// 	slidesToShow: 1,
 		// 	slidesToScroll: 1,
-		// 	arrows: false,
-		// 	dots: false,
-		// 	adaptiveHeight: true,
-		// 	asNavFor: '.js-menu-paging'
-		// });
-
-		// $('.js-menu-paging').slick({
-		// 	slidesToShow: 3,
-		// 	slidesToScroll: 1,
-		// 	asNavFor: '.js-menu-pages',
+		// 	asNavFor: '#slider-' + target,
 		// 	dots: false,
 		// 	arrows: true,
 		// 	centerMode: true,
+		// 	infinite: false,
 		// 	focusOnSelect: true,
+		// 	// initialSlide: 1,
+		// 	variableWidth: true,
 		// 	mobileFirst : true,
 		// 	responsive : [
 		// 		{
+		// 			breakpoint : 480,
+		// 			settings : {
+		// 				slidesToShow: 3,
+		// 				centerMode: false,
+		// 			}
+		// 		},
+		// 		{
+		// 			breakpoint : 640, //medium breakpoint
+		// 			settings : {
+		// 				slidesToShow: 4,
+		// 				centerMode: false,
+		// 			}
+		// 		},
+		// 		{
 		// 			breakpoint : 840,
 		// 			settings : {
-		// 				slidesToShow: 15,
+		// 				//bad, but working version: centermode false, slidesToShow 9, arrows false
+		// 				//when arrows true, there are two arrows (button elements) generated in slick track and they are considered slides and they desynchronize synced sliders
 		// 				centerMode: false,
+		// 				slidesToShow: 9,
+		// 				arrows: false,
 		// 			}
 		// 		}
 		// 	]
 		// });
+		$paginationSlider.slick( paginationSliderOptions );
 	}
 });
